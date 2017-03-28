da4923939803b224317592826579e133ecce483c
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-03-28 04:10:57 +0100

    got it closer to the format, had to move subject-data so it could be found

--- API/src/public/index.php
+++ API/src/public/index.php
@@ -10,14 +10,30 @@ require '../vendor/autoload.php';
 //making the app
 $app = new \Slim\App;
 
-//adding a route fot student-data
+//adding a route for student-data
 $app->get('/student-data/{id}', function (Request $request, Response $response) {
   $id = $request->getAttribute('id');
-  $file="subject-data.csv";
-  $csv= file_get_contents($file);
-  $array = array_map("str_getcsv", explode("\n", $csv));
-  $json = json_encode($array);
-  print_r($json);
+
+-----//getting the csv data----------
+
+//opening the csv file in read-only
+$file = fopen('subject-data.csv', 'r');
+
+//getting the headers to be used as keys
+$keyName  = fgetcsv($file,",");
+
+$json = array();
+
+//bind each relevant keyName to a it's corresponding value
+while ($row = fgetcsv($file, ",")) {
+        $json[] = array_combine($keyName, $row);
+    }
+
+//encodes the array into JSON format
+$json = json_encode($json);
+
+//prints the JSON
+print_r($json);
   $response->getBody()->write('');
   return $response;
 });
