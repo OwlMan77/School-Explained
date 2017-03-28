9ed3a5f80f53b7b95b2173eeec69a619dbfa8ed2
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-03-28 04:09:53 +0100

    was able to figure out how get csv data and make it JSON

--- API/src/public/index.php
+++ API/src/public/index.php
@@ -13,8 +13,12 @@ $app = new \Slim\App;
 //adding a route fot student-data
 $app->get('/student-data/{id}', function (Request $request, Response $response) {
   $id = $request->getAttribute('id');
-  $response->getBody()->write("{ subjectId:$id}");
-
+  $file="subject-data.csv";
+  $csv= file_get_contents($file);
+  $array = array_map("str_getcsv", explode("\n", $csv));
+  $json = json_encode($array);
+  print_r($json);
+  $response->getBody()->write('');
   return $response;
 });
 $app->run();
