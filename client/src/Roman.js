73ded49595c84dc599072151647bb8c27d04b476
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-04-03 16:08:07 +0100

    added comments

--- client/src/Roman.js
+++ client/src/Roman.js
@@ -9,12 +9,14 @@ import React, { Component } from 'react';
       else if (num < 100 && num > 0) {
         //turn the number into a string
         var digits = String(+num).split(""),
+        //sets an array of values to build the solution roman numeral
             key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
                    "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
                    "","I","II","III","IV","V","VI","VII","VIII","IX"],
             roman = "",
             i = 3;
         while (i--)
+        //builds the roman numeral by taking each digit of the input and getting the corresponding numeral
             roman = (key[+digits.pop() + (i * 10)] || "") + roman;
         return Array(+digits.join("") + 1).join("M") + roman;
       }
@@ -35,10 +37,13 @@ class romanNumerals extends Component {
    this.handleSubmit = this.handleSubmit.bind(this);
  }
  handleChange(e){
+   //set value to be the value in the field
   this.setState({value: e.target.value});
  }
  handleSubmit(e){
+   //stops the page from refreshing
    e.preventDefault();
+   //sets the answer to be the roman numeral
    this.setState({numeral: romanize(this.state.value)});
  }
 //create a form field, a button and a output
