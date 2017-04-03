323e9b9aa7ae62f103a742ab0ac4af07b12fb438
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-04-03 15:42:18 +0100

    finished the second part

--- client/src/Roman.js
+++ client/src/Roman.js
@@ -1,35 +1,56 @@
 import React, { Component } from 'react';
 
-class romanNumerals extends Component {
-//create a form field, a button and a output
+// create a function that will convert number into roman numerals
 
-//create a function that will convert number into roman numerals
-  // romanize (num) {
-  //     if (!+num)
-  //         return false;
-  //     else if (num < 100) {
-  //       var digits = String(+num).split(""),
-  //           key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-  //                  "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-  //                  "","I","II","III","IV","V","VI","VII","VIII","IX"],
-  //           roman = "",
-  //           i = 3;
-  //       while (i--)
-  //           roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-  //       return Array(+digits.join("") + 1).join("M") + roman;
-  //     }
-  //     else{
-  //       console.log('over limit');
-  //     }
-  //
-  // }
+  function romanize (num) {
+      if (!+num)
+          return false;
+      else if (num < 100 && num > 0) {
+        var digits = String(+num).split(""),
+            key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                   "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                   "","I","II","III","IV","V","VI","VII","VIII","IX"],
+            roman = "",
+            i = 3;
+        while (i--)
+            roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+        return Array(+digits.join("") + 1).join("M") + roman;
+      }
+      else{
+      return "Please choose a number between 1-99";
+      }
+
+  }
+
+
+class romanNumerals extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {value: '', numeral: 'Type in a number'};
+
+   this.handleChange = this.handleChange.bind(this);
+   this.handleSubmit = this.handleSubmit.bind(this);
+ }
+ handleChange(e){
+  this.setState({value: e.target.value});
+ }
+ handleSubmit(e){
+   e.preventDefault();
+   this.setState({numeral: romanize(this.state.value)});
+ }
+//create a form field, a button and a output
   //display answer
   render(){
     return(
       <div>
-      <p>empty Div</p>
+      <p>Roman Numeral Generator</p>
+      <form onSubmit={this.handleSubmit}>
+      <input type="number" value={this.state.value} onChange={this.handleChange}></input>
+      <input type="submit" value="Covert"></input>
+      <p>{this.state.numeral}</p>
+      </form>
       </div>
-    )
+    );
   }
 }
 
